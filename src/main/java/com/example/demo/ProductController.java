1dd6caf06aa0f748068e6ea5cd57d4fe463323c9
dinicolae <dinu.nicolae@endava.com>
2019-07-17 11:12:41 +0300

    first commit

---
+++ src/main/java/com/example/demo/ProductController.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import com.example.demo.Services.ProductService;
+import com.example.demo.domain.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller()
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping("/")
+    public String home(Model model) {
+        List<Product> listProducts = productService.listAll();
+        model.addAttribute("listProducts", listProducts);
+        model.addAttribute("name", "Prod Manager");
+        return "index";
+    }
+
+    @RequestMapping("/new")
+    public String showNewProductForm(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
+        return "newProduct";
+    }
+
+    @PostMapping("/save")
+    public String saveProduct(@ModelAttribute("product")Product product){
+        productService.save(product);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditPage(@PathVariable(name = "id") Long id){
+        ModelAndView mv = new ModelAndView("editProduct");
+        Product product = productService.get(id);
+        mv.addObject("product", product);
+        return mv;
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable Long id){
+        productService.delete(id);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/example/demo/domain/Product.java
@@ -0,0 +1,68 @@
+package com.example.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String brand;
+    private String madeIn;
+    private Float price;
+
+//    public Product(){
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getBrand() {
+//        return brand;
+//    }
+//
+//    public void setBrand(String brand) {
+//        this.brand = brand;
+//    }
+//
+//    public String getMadeIn() {
+//        return madeIn;
+//    }
+//
+//    public void setMadeIn(String madeIn) {
+//        this.madeIn = madeIn;
+//    }
+//
+//    public Float getPrice() {
+//        return price;
+//    }
+//
+//    public void setPrice(Float price) {
+//        this.price = price;
+//    }
+}
