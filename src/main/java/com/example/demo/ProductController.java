a5faacd5b0794ab8914a16f287127a54f2a28a94
dinicolae <dinu.nicolae@endava.com>
2019-07-23 11:54:02 +0300

    new docker

--- src/main/java/com/example/demo/ProductController.java
+++ src/main/java/com/example/demo/ProductController.java
@@ -10,7 +10,7 @@ public class ProductController {
     @RequestMapping("/")
     @ResponseBody
     String printHello() {
-        return "Hello Dinu";
+        return "Hello Team";
     }
 //    private ProductService productService;
 //
