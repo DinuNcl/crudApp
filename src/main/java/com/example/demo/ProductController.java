ee33e65f383fbd66861aea85b5d9d1f12bf594d0
dinicolae <dinu.nicolae@endava.com>
2019-09-02 12:54:56 +0300

    change file name

--- src/main/java/com/example/demo/ProductController.java
+++ src/main/java/com/example/demo/ProductController.java
@@ -10,7 +10,7 @@ public class ProductController {
     @RequestMapping("/")
     @ResponseBody
     String printHello() {
-        return "new";
+        return "test";
     }
 //    private ProductService productService;
 //
