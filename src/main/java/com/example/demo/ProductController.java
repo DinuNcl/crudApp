1e97369bb80520d5f04571b3f5396e4341c2c57e
DinuNcl <37775427+DinuNcl@users.noreply.github.com>
2019-09-03 09:20:44 +0300

    Update ProductController.java

--- src/main/java/com/example/demo/ProductController.java
+++ src/main/java/com/example/demo/ProductController.java
@@ -10,7 +10,7 @@ public class ProductController {
     @RequestMapping("/")
     @ResponseBody
     String printHello() {
-        return "test";
+        return "prod test";
     }
 //    private ProductService productService;
 //
